81ed44384be114b3d008e66c1dfd889fb1bdcc34
Heinrich <smithheinrich254@gmail.com>
2022-12-24 18:43:40 +0300

    database

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Heinirich/golang_api_test
 
 go 1.18
+
+require github.com/go-sql-driver/mysql v1.7.0 // indirect
--- model/connect.go
+++ model/connect.go
@@ -4,10 +4,12 @@ import (
 	"database/sql"
 	"log"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+
 )
 
 func Connect() *sql.DB {
-	db,err := sql.Open("mysql","root:Darkweb360!!@(tcp:localhost:3306")
+	db,err := sql.Open("mysql","root:Darkweb360!!@/golang_api_test")
 
 	if err != nil{
 		log.Fatal(err)
