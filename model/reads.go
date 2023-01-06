daba6f05551f8e2f6f77507be59728017fb656fe
Heinrich <smithheinrich254@gmail.com>
2023-01-06 20:51:02 +0300

    Modified Imports

--- model/reads.go
+++ model/reads.go
@@ -1,18 +1,19 @@
 package model
 
-import "fmt"
+import "github.com/Heinirich/golang_api_test/views"
 
 func ReadAll()([]views.PostRequest,error){
 	rows,err := con.Query("SELCET * FROM TODO")
 	if err !=nil{
 		return nil,err
 	} 
-	fmt.Println("select all")
+	
 	todo := []views.PostRequest{}
 	for rows.Next(){
 		data := views.PostRequest{}
 		rows.Scan(&data.Name,&data.Todo)
 		todos = append(todos,data)
 	}
+	
 	return todos,nil
 }
