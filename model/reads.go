05741b4e75843bf7de2f5efeeaf22f09acdab6de
Heinrich <smithheinrich254@gmail.com>
2023-01-06 20:55:52 +0300

    Fixes

--- model/reads.go
+++ model/reads.go
@@ -3,12 +3,12 @@ package model
 import "github.com/Heinirich/golang_api_test/views"
 
 func ReadAll()([]views.PostRequest,error){
-	rows,err := con.Query("SELCET * FROM TODO")
+	rows,err := con.Query("SELECT * FROM todo")
 	if err !=nil{
 		return nil,err
 	} 
 	
-	todo := []views.PostRequest{}
+	todos := []views.PostRequest{}
 	for rows.Next(){
 		data := views.PostRequest{}
 		rows.Scan(&data.Name,&data.Todo)
